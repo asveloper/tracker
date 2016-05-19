3d56c198adf5a5200242a44fd65df20407c93b49
Arslan Nasir <arslan@mobylogix.com>
2016-05-19 11:26:22 +0300

    Updated for IOS as well

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,43 @@
+var React = require('react');
 var ReactNative = require('react-native');
+var Button = require('react-native-button');
 
 var {
-  AppRegistry
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ToastAndroid
 } = ReactNative;
 
-import { Geolocation } from './app/geolocation.js';
+import { Geolocation } from './components/geolocation.js';
 
-AppRegistry.registerComponent('KoTacTracker', () => Geolocation);
+var KoTacTracker = React.createClass({
+  getInitialState(){
+    return{
+      geolocation: false,
+    }
+  },
+  _handlePress(event){
+    ToastAndroid.show('Start Pressed!', ToastAndroid.LONG);
+    this.setState({geolocation: true});
+  },
+  render(){
+
+    if(this.state.geolocation){
+      return <Geolocation />;
+    }
+
+    return(
+      <Button
+        style={{fontSize: 20, color: 'green', marginTop: 50}}
+        styleDisabled={{color: 'red'}}
+        onPress={this._handlePress}
+      >
+        Start!
+      </Button>
+    );
+  },
+});
+
+AppRegistry.registerComponent('KoTacTracker', () => KoTacTracker);

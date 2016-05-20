b3655792acf4dc878543609b01cdb5ff90d11f50
Arslan Nasir <arslan@mobylogix.com>
2016-05-20 11:42:06 +0300

    Android home updated

--- index.android.js
+++ index.android.js
@@ -1,3 +1,9 @@
+var ReactNative = require('react-native');
+
+var {
+  AppRegistry,
+} = ReactNative;
+
 import { Login } from './components/login.js';
 
 AppRegistry.registerComponent('KoTacTracker', () => Login);

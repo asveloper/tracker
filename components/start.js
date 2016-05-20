41af4570dcfd1c5c3a8e7a14c87c626bc01b7f40
Arslan Nasir <arslan@mobylogix.com>
2016-05-20 11:04:27 +0300

    Start components moved

---
+++ components/start.js
@@ -0,0 +1,37 @@
+var React = require('react');
+var ReactNative = require('react-native');
+var Button = require('react-native-button');
+
+var {
+  ToastAndroid
+} = ReactNative;
+
+import { Geolocation } from './components/geolocation.js';
+
+export const Start = React.createClass({
+  getInitialState(){
+    return{
+      geolocation: false,
+    }
+  },
+  _handlePress(event){
+    ToastAndroid.show('Start Pressed!', ToastAndroid.LONG);
+    this.setState({geolocation: true});
+  },
+  render(){
+
+    if(this.state.geolocation){
+      return <Geolocation />;
+    }
+
+    return(
+      <Button
+        style={{fontSize: 20, color: 'green', marginTop: 50}}
+        styleDisabled={{color: 'red'}}
+        onPress={this._handlePress}
+      >
+        Start!
+      </Button>
+    );
+  },
+});

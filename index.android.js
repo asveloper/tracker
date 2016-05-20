709e41c8edfb7c637f19661895650abb225f9818
Arslan Nasir <arslan@mobylogix.com>
2016-05-20 11:08:39 +0300

    Android login page updated

--- index.android.js
+++ index.android.js
@@ -5,27 +5,33 @@ var Config = require('./config');
 
 var {
   AppRegistry,
+  AsyncStorage,
   StyleSheet,
   Text,
   View,
   ToastAndroid
 } = ReactNative;
 
-import { Geolocation } from './components/geolocation.js';
+import { Start } from './components/start.js';
 
 const REQUEST_URL = Config.SERVER_URL.concat(Config.LOGIN_PATH);
 
 var KoTacTracker = React.createClass({
   getInitialState(){
     return{
-      geolocation: false,
+      start: false,
     }
   },
-  _handlePress(event){
-    ToastAndroid.show('Start Pressed!', ToastAndroid.LONG);
-    this.setState({geolocation: true});
+  _handleLogin(event){
+    ToastAndroid.show('Login Pressed!', ToastAndroid.LONG);
+
+    // TODO: send form creds and fetch data
+
+    // TODO: Store authToken and userId in AsyncStorage
+
+    this.setState({start: true});
   },
-  fetchData: function(){
+  fetchData: function(email, password){
     fetch(REQUEST_URL, {
       method: 'POST',
       headers: {
@@ -33,8 +39,8 @@ var KoTacTracker = React.createClass({
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        email: "test@kotactracker.com",
-        password: "password"
+        email: email,
+        password: password
       })
     })
     .then((response) => response.json())
@@ -43,22 +49,19 @@ var KoTacTracker = React.createClass({
     })
     .done();
   },
-  componentDidMount: function(){
-    this.fetchData();
-  },
   render(){
 
-    if(this.state.geolocation){
-      return <Geolocation />;
+    if(this.state.start){
+      return <Start />;
     }
 
     return(
       <Button
         style={{fontSize: 20, color: 'green', marginTop: 50}}
         styleDisabled={{color: 'red'}}
-        onPress={this._handlePress}
+        onPress={this._handleLogin}
       >
-        Start!
+        Login!
       </Button>
     );
   },

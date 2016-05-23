679db332462a3a4bf4c5517ed288366f52269e81
Arslan Nasir <arslan@mobylogix.com>
2016-05-23 11:05:25 +0300

    Map View component added

--- components/geolocation.js
+++ components/geolocation.js
@@ -1,10 +1,11 @@
 var React = require('react');
 var ReactNative = require('react-native');
+var MapView = require('react-native-maps');
+
 var {
   StyleSheet,
   Text,
   View,
-  MapView
 } = ReactNative;
 
 export const Geolocation = React.createClass({
@@ -14,6 +15,12 @@ export const Geolocation = React.createClass({
     return {
       initialPosition: 'unknown',
       lastPosition: 'unknown',
+      isFirstLoad: true,
+      mapRegion: {
+        latitude: 0,
+        longitude: 0
+      },
+      annotations: [],
     };
   },
 
@@ -29,6 +36,12 @@ export const Geolocation = React.createClass({
     this.watchID = navigator.geolocation.watchPosition((position) => {
       var lastPosition = JSON.stringify(position);
       this.setState({lastPosition});
+      this.setState({
+        region: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        }
+      });
     });
   },
 
@@ -39,6 +52,14 @@ export const Geolocation = React.createClass({
   render: function() {
     return (
       <View>
+        <MapView
+          style={styles.map}
+          onRegionChange={this._onRegionChange}
+          onRegionChangeComplete={this._onRegionChangeComplete}
+          region={this.state.mapRegion}
+          annotations={this.state.annotations}
+        />
+
         <Text>
           <Text style={styles.title}>Initial position: </Text>
           {this.state.initialPosition}
@@ -49,11 +70,60 @@ export const Geolocation = React.createClass({
         </Text>
       </View>
     );
-  }
+  },
+
+  _getAnnotations(region) {
+    return [{
+      longitude: region.longitude,
+      latitude: region.latitude,
+      title: 'You Are Here',
+    }];
+  },
+
+  _onRegionChange(region) {
+    this.setState({
+      mapRegion: region,
+    });
+  },
+
+  _onRegionChangeComplete(region) {
+    if (this.state.isFirstLoad) {
+      this.setState({
+        mapRegion: region,
+        annotations: this._getAnnotations(region),
+        isFirstLoad: false,
+      });
+    }
+  },
 });
 
 var styles = StyleSheet.create({
   title: {
     fontWeight: '500',
+  },
+  map: {
+    height: 150,
+    margin: 10,
+    borderWidth: 1,
+    borderColor: '#000000',
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textInput: {
+    width: 150,
+    height: 20,
+    borderWidth: 0.5,
+    borderColor: '#aaaaaa',
+    fontSize: 13,
+    padding: 4,
+  },
+  changeButton: {
+    alignSelf: 'center',
+    marginTop: 5,
+    padding: 3,
+    borderWidth: 0.5,
+    borderColor: '#777777',
   },
 });

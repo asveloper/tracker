0db476194846dcc7811e5431e6a65c0bd8597fc8
Arslan Nasir <arslan@mobylogix.com>
2016-05-24 23:59:31 +0300

    Added functionality to store title on server db and store trip id in local db

--- components/trip_title.js
+++ components/trip_title.js
@@ -1,10 +1,12 @@
 var React = require('react');
 var ReactNative = require('react-native');
 var Button = require('react-native-button');
+var Config = require('../config');
 
 var {
   ToastAndroid,
   StyleSheet,
+  AsyncStorage,
   View,
   Text,
   TextInput,
@@ -13,23 +15,73 @@ var {
 import { Geolocation } from './geolocation.js';
 import { StartTracking } from './start_tracking.js';
 
+let REQUEST_URL = Config.SERVER_URL.concat(Config.TRIP_PATH);
+
 export const TripTitle = React.createClass({
   getInitialState() {
     return {
       geolocation: false,
-      startTracking: false
+      startTracking: false,
+      authToken: undefined,
+      userId: undefined,
+      title: undefined
     };
+  },
+  componentDidMount(){
+    AsyncStorage.getItem('authToken', (err, authToken) => {
+      if(err){
+        console.log(err);
+      }else{
+        this.setState({authToken: authToken});
+      }
+    });
+    AsyncStorage.getItem('userId', (err, userId) => {
+      if(err){
+        console.log(err);
+      }else{
+        this.setState({userId: userId});
+      }
+    });
   },
 
   _handleStart(event) {
-
-    //TODO: Send title to server and store _id in AsyncStorage
-
-    this.setState({ geolocation: true });
+    console.log("Start called");
+    let title = this.refs.title.props.value;
+    this.saveTrip(title);
   },
 
   _handleCancel(event){
     this.setState({ startTracking: true });
+  },
+
+  saveTrip: function(title){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = (e) => {
+      if (request.readyState !== 4) {
+        return;
+      }
+
+      if (request.status > 210) {
+        console.warn('Error occured');
+        console.log(responseData);
+      } else {
+        let responseData = JSON.parse(request.responseText);
+
+        if(responseData.status == "success"){
+          AsyncStorage.setItem('tripId', responseData.data._id);
+
+          this.setState({geolocation: true});
+        }
+      }
+    };
+
+    let params = "title="+title+"&createdBy="+this.state.userId;
+    request.open('POST', REQUEST_URL);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.setRequestHeader("X-Auth-Token", this.state.authToken);
+    request.setRequestHeader("X-User-Id", this.state.userId);
+    request.send(params);
+
   },
 
   render() {
@@ -51,6 +103,8 @@ export const TripTitle = React.createClass({
               autoFocus={true}
               keyboardType='default'
               placeholder='Title'
+              onChangeText={(title) => this.setState({title})}
+              value={this.state.title}
               ref="title"
             />
 
@@ -94,7 +148,7 @@ var styles = StyleSheet.create({
   button: {
     borderRadius: 5,
     flex: 1,
-    height: 44,
+    height: 50,
     alignSelf: 'stretch',
     justifyContent: 'center',
     overflow: 'hidden',

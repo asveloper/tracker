8059d2b1e720279e8a48d77ec8881a16da93f4b9
Arslan Nasir <arslan@mobylogix.com>
2016-05-25 09:45:00 +0300

    Added functionality to save latitude and longitude of current location on server db

--- components/geolocation.js
+++ components/geolocation.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactNative = require('react-native');
 var MapView = require('react-native-maps');
+var Config = require('../config');
 
 var {
   StyleSheet,
@@ -9,6 +10,7 @@ var {
   Dimensions,
   Platform,
   TouchableOpacity,
+  AsyncStorage,
 } = ReactNative;
 
 var screen = Dimensions.get('window');
@@ -19,11 +21,16 @@ const LONGITUDE = -122.4324;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
+let REQUEST_URL = Config.SERVER_URL.concat(Config.LATLNG_PATH);
+
 export const Geolocation = React.createClass({
   watchID: (null: ?number),
 
   getInitialState: function() {
     return {
+      authToken: undefined,
+      userId: undefined,
+      tripId: undefined,
       initialPosition: 'unknown',
       lastPosition: 'unknown',
       region: {
@@ -36,28 +43,80 @@ export const Geolocation = React.createClass({
   },
 
   componentDidMount: function() {
-    // navigator.geolocation.getCurrentPosition(
-    //   (position) => {
-    //     var initialPosition = JSON.stringify(position);
-    //     this.setState({initialPosition});
-    //   },
-    //   (error) => alert(error.message),
-    //   {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
-    // );
-    // this.watchID = navigator.geolocation.watchPosition((position) => {
-    //   var lastPosition = JSON.stringify(position);
-    //   this.setState({lastPosition});
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var initialPosition = JSON.stringify(position);
+        this.setState({initialPosition});
+      },
+      (error) => alert(error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lastPosition = JSON.stringify(position);
+      this.setState({lastPosition});
       // this.setState({
       //   region: {
       //     latitude: position.coords.latitude,
       //     longitude: position.coords.longitude
       //   }
       // });
-    // });
+    });
+
+    AsyncStorage.getItem('authToken', (err, authToken) => {
+      if(err){
+        console.log(err);
+      }else{
+        this.setState({authToken: authToken});
+      }
+    });
+    AsyncStorage.getItem('userId', (err, userId) => {
+      if(err){
+        console.log(err);
+      }else{
+        this.setState({userId: userId});
+      }
+    });
+    AsyncStorage.getItem('tripId', (err, tripId) => {
+      if(err){
+        console.log(err);
+      }else{
+        this.setState({tripId: tripId});
+      }
+    });
+
+
   },
 
   componentWillUnmount: function() {
     navigator.geolocation.clearWatch(this.watchID);
+  },
+
+  saveLatLng: function(latitude, longitude){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = (e) => {
+      if (request.readyState !== 4) {
+        return;
+      }
+
+      if (request.status > 210) {
+        console.warn('Error occured');
+        console.log(responseData);
+      } else {
+        let responseData = JSON.parse(request.responseText);
+
+        if(responseData.status == "success"){
+          console.log(responseData);
+        }
+      }
+    };
+
+    let params = "latitude="+latitude+"&longitude="+longitude+"&tripId="+this.state.tripId+"&createdBy="+this.state.userId;
+    request.open('POST', REQUEST_URL);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.setRequestHeader("X-Auth-Token", this.state.authToken);
+    request.setRequestHeader("X-User-Id", this.state.userId);
+    request.send(params);
+
   },
 
   render: function() {

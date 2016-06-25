1f99a319e8901594c402631af408479e6228f972
Arslan Nasir <arslan@mobylogix.com>
2016-06-25 22:16:24 +0500

    DirectionsService component added

---
+++ components/directions_service.js
@@ -0,0 +1,31 @@
+var React = require('react');
+var ReactNative = require('react-native');
+var GoogleMapsLoader = require('google-maps');
+
+var {
+  View,
+  StyleSheet,
+} = ReactNative;
+
+export const DirectionsService = React.createClass({
+  getInitialState(){
+    return{
+    }
+  },
+
+  componentDidMount(){
+  },
+
+  render(){
+
+    return(
+      <View style={styles.container}>
+      </View>
+    );
+  },
+});
+
+var styles = StyleSheet.create({
+  container: {
+  }
+});

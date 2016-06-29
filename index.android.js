cecf2a2f19af26346b962ff4bedf5e72ed3a95f4
Arslan Nasir <arslan@mobylogix.com>
2016-06-29 12:34:14 +0500

    DDP connection added

--- index.android.js
+++ index.android.js
@@ -1,8 +1,16 @@
-'use strict';
+var Config = require("./config");
 
 import {
   AppRegistry,
 } from 'react-native';
+
+import Meteor from 'react-native-meteor';
+
+Meteor.connect(Config.SOCKET_URL);
+
+Meteor.ddp.on('connected', () => {
+  console.log("DDP connected");
+});
 
 import { Login } from './components/login.js';
 

505afa53564bc7d7a7f9a5510793f8ba83afb425
Arslan Nasir <arslan@mobylogix.com>
2016-06-29 16:08:10 +0500

    Trips component updated

--- components/trips.js
+++ components/trips.js
@@ -1,114 +1,31 @@
-'use strict';
+import React, { Component } from 'react';
 
-var React = require('react');
-var ReactNative = require('react-native');
-var Config = require('../config');
-
-var {
+import {
   StyleSheet,
   Text,
   View,
-  ListView,
-  AsyncStorage,
-  BackAndroid,
-} = ReactNative;
+} from 'react-native';
 
-import { Dashboard } from './dashboard.js';
+import Meteor,  { createContainer, MeteorComplexListView } from 'react-native-meteor';
 
-var REQUEST_URL = Config.SERVER_URL.concat(Config.USER_TRIPS_PATH);
+class Trips extends Component {
+  constructor(props){
+    super(props);
 
-export const Trips = React.createClass({
-  getInitialState: function(){
-    return{
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2
-      }),
-      loaded: false,
-      authToken: undefined,
-      userId: undefined,
-      dashboard: false
-    }
-  },
-
-  fetchData: function(){
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = (e) => {
-      if (request.readyState !== 4) {
-        return;
-      }
-
-      if (request.status > 210) {
-        console.warn('Error occured');
-        console.log(responseData);
-      } else {
-        let responseData = JSON.parse(request.responseText);
-
-        if(responseData.status == "success"){
-          this.setState({
-            dataSource: this.state.dataSource.cloneWithRows(responseData.data),
-            loaded: true
-          });
-        }
-      }
+    this.state = {
     };
 
-    request.open('GET', REQUEST_URL);
-    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    request.setRequestHeader("X-Auth-Token", this.state.authToken);
-    request.setRequestHeader("X-User-Id", this.state.userId);
-    request.send();
+    // bind functions here
+  }
 
-  },
+  componentDidMount(){
+    // console.log(Meteor.collection('trips').find().count());
+  }
 
-  componentDidMount: function(){
-    AsyncStorage.getItem('authToken', (err, authToken) => {
-      if(err){
-        console.log(err);
-      }else{
-        this.setState({authToken: authToken});
-      }
-    });
-    AsyncStorage.getItem('userId', (err, userId) => {
-      if(err){
-        console.log(err);
-      }else{
-        this.setState({userId: userId});
-        this.fetchData();
-      }
-    });
-
-    BackAndroid.addEventListener('hardwareBackPress', this.backAndroidHandler);
-  },
-
-  componentWillUnmount: function() {
-    BackAndroid.removeEventListener('hardwareBackPress', this.backAndroidHandler);
-  },
-
-  backAndroidHandler: function() {
-    this.setState({dashboard: true});
-    return true;
-  },
-
-  render: function() {
-
-    if(this.state.dashboard){
-      return <Dashboard />;
-    }
-
-    return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderTrip}
-        style={styles.listView}
-        renderSeparator={(sectionID, rowID) => <View key={`${sectionID}-${rowID}`} style={styles.separator} />}
-      />
-    );
-  },
-
-  renderTrip: function(trip){
+  renderRow(trip){
     let date = new Date(trip.createdAt);
     let day = date.getDate(); //Date of the month: 2 in our example
-    let month = date.getMonth(); //Month of the Year: 0-based index, so 1 in our example
+    let month = date.getMonth() + 1; //Month of the Year: 0-based index, so 1 in our example
     let year = date.getFullYear(); //Year: 2013
 
     return(
@@ -122,7 +39,43 @@ export const Trips = React.createClass({
       </View>
     );
   }
-});
+
+  render() {
+
+    const { tripsReady } = this.props;
+
+    return (
+
+
+      <MeteorComplexListView
+        elements={()=>{return Meteor.collection('trips').find({createdBy: Meteor.userId()}, {sort: {createdAt: -1}})}}
+        renderRow={this.renderRow}
+        enableEmptySections={true}
+        style={styles.listView}
+        renderSeparator={(sectionID, rowID) => <View key={`${sectionID}-${rowID}`} style={styles.separator} />}
+      />
+    );
+  }
+
+}
+
+Trips.propTypes = {
+
+};
+
+Trips.defaultProps = {
+
+};
+
+export default createContainer(params => {
+  const handle = Meteor.subscribe('trips');
+
+  return {
+    tripsReady: handle.ready(),
+    trips: Meteor.collection('trips').find({createdBy: Meteor.userId()}, {sort: {createdAt: -1}})
+  };
+}, Trips);
+
 
 var styles = StyleSheet.create({
   listView: {

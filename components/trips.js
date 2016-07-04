9b3d71196a29097a43c8428f049a2071c81bf248
Arslan Nasir <arslan@mobylogix.com>
2016-07-04 06:07:33 +0500

    Trips component updated

--- components/trips.js
+++ components/trips.js
@@ -4,18 +4,38 @@ import {
   StyleSheet,
   Text,
   View,
+  TouchableHighlight,
 } from 'react-native';
 
 import Meteor,  { createContainer, MeteorComplexListView } from 'react-native-meteor';
+
+import Details from './details.js';
 
 class Trips extends Component {
   constructor(props){
     super(props);
 
     this.state = {
+      details: false,
+      tripId: undefined
     };
 
     // bind functions here
+  }
+
+  componentWillUnmount(){
+    console.log("trips component mounted");
+    this.setState({
+      details: false,
+      tripId: undefined
+    });
+  }
+
+  showDetails(trip){
+    this.setState({
+      tripId: trip._id,
+      details: true
+    });
   }
 
   renderHeader(){
@@ -32,20 +52,23 @@ class Trips extends Component {
   }
 
   renderRow(trip){
+
     let date = new Date(trip.createdAt);
     let day = date.getDate(); //Date of the month: 2 in our example
     let month = date.getMonth() + 1; //Month of the Year: 0-based index, so 1 in our example
     let year = date.getFullYear(); //Year: 2013
 
     return(
-      <View style={styles.container}>
-        <View style={styles.dateColumn}>
-          <Text>{day}-{month}-{year}</Text>
+      <TouchableHighlight onPress={() => this.showDetails(trip)}>
+        <View style={styles.container}>
+          <View style={styles.dateColumn}>
+            <Text>{day}-{month}-{year}</Text>
+          </View>
+          <View style={styles.distanceColumn}>
+            <Text>{trip.distance} KM</Text>
+          </View>
         </View>
-        <View style={styles.distanceColumn}>
-          <Text>{trip.distance} KM</Text>
-        </View>
-      </View>
+      </TouchableHighlight>
     );
   }
 
@@ -53,13 +76,16 @@ class Trips extends Component {
 
     const { tripsReady } = this.props;
 
-    return (
+    if(this.state.details){
+      return <Details tripId={this.state.tripId} />;
+    }
 
+    return (
 
       <MeteorComplexListView
         elements={()=>{return Meteor.collection('trips').find({createdBy: Meteor.userId()}, {sort: {createdAt: -1}})}}
         renderHeader={this.renderHeader}
-        renderRow={this.renderRow}
+        renderRow={this.renderRow.bind(this)}
         enableEmptySections={true}
         style={styles.listView}
         renderSeparator={(sectionID, rowID) => <View key={`${sectionID}-${rowID}`} style={styles.separator} />}

667978904b625e6b8d0a74a48d4ad113d0ce1579
Arslan Nasir <arslan@mobylogix.com>
2016-07-05 08:37:55 +0500

    Dates module removed

--- components/modules/dates.js
+++
@@ -1,9 +0,0 @@
-export default DateFromTimestamp = function(d){
-  let date = new Date(d);
-  let day = date.getDate(); //Date of the month: 2 in our example
-  let month = date.getMonth() + 1; //Month of the Year: 0-based index, so 1 in our example
-  let year = date.getFullYear(); //Year: 2013
-
-  return day+"-"+month+"-"+year;
-}
-

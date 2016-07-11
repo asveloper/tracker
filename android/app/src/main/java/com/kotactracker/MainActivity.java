b6a8d6184a1664e04dec38524aefe23dd7c52273
Arslan Nasir <arslan@mobylogix.com>
2016-07-12 00:24:49 +0500

    Packages updated

--- android/app/src/main/java/com/kotactracker/MainActivity.java
+++ android/app/src/main/java/com/kotactracker/MainActivity.java
@@ -1,6 +1,8 @@
 package com.kotactracker;
 
 import com.facebook.react.ReactActivity;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.mapbox.reactnativemapboxgl.ReactNativeMapboxGLPackage;
 import com.AirMaps.AirPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -36,6 +38,8 @@ public class MainActivity extends ReactActivity {
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
+            new VectorIconsPackage(),
+            new ReactNativeMapboxGLPackage(),
             new AirPackage()
         );
     }

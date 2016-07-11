3d0014f8c99b9f73cae526945c03c3aea0fb3f65
Arslan Nasir <arslan@mobylogix.com>
2016-07-11 15:47:07 +0500

    Drawer layout package added

--- components/dashboard.js
+++ components/dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Meteor, { createContainer } from 'react-native-meteor';
 import Button from 'apsl-react-native-button';
 import MaterialSwitch from 'react-native-material-design-switch';
+import DrawerLayout from 'react-native-drawer-layout';
 
 import {
   Platform,
